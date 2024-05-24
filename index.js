e4260392dced046036c92f26cb139632a85946cd
hiroshi nagao <hi_1205@hotmail.com>
2024-05-24 12:36:24 +0900

    mini project rock paper scissors

---
+++ index.js
@@ -0,0 +1,72 @@
+console.log('hi');
+
+const getUserChoice = userInput => {
+  userInput = userInput.toLowerCase()
+  if (userInput === 'rock' || userInput === 'paper' ||userInput === 'scissors' || userInput === 'bomb') {
+    return userInput
+  } else {
+    console.log('Invalid Input');
+  }
+};
+
+const getComputerChoice = () => {
+  const randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+      return 'rock';
+    case 1:
+      return 'paper';
+    case 2:
+      return 'scissors';
+  }
+};
+
+//console.log(getComputerChoice());
+const determineWinner = (userChoice, computerChoice) => {
+  if (userChoice === computerChoice) {
+    return 'It is a tie!'
+  }
+  if (userChoice === 'rock') {
+    if (computerChoice == 'paper') {
+      return 'The computer won...'
+    } else {
+      return 'You won!'
+    }
+  }
+
+  if (userChoice === 'paper') {
+    if (computerChoice === 'scissors') {
+      return "The computer won..."
+    } else {
+      return 'You won!'
+    }
+  }
+
+  if (userChoice === 'scissors') {
+    if (computerChoice === 'rock') {
+      return "The computer won..."
+    } else {
+      return 'You won!'
+    }
+  }
+
+  if (userChoice == 'bomb') {
+    return "You won!!!!!"
+  }
+};
+
+//console.log(determineWinner('rock', 'paper'));
+const playGame = () => {
+  const userChoice = getUserChoice('bomb');
+  const computerChoice = getComputerChoice();
+  console.log(`You threw: ${userChoice}`);
+  console.log(`The computer threw: ${computerChoice}`);
+
+  console.log(determineWinner(userChoice, computerChoice));
+};
+
+
+
+
+playGame()
+
